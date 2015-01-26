0f696d178c5ebca6a4716c76a20e1ab816473066
Даниил <\>
2015-01-26 14:30:26 +0300

    Добавил возможность управления персонажем

--- Assets/scripts/HeroController.cs
+++ Assets/scripts/HeroController.cs
@@ -3,13 +3,51 @@ using System.Collections;
 
 public class HeroController : MonoBehaviour {
 
+	private float speedHero=15f;
+	private bool flipright=true;
+	private float moves=0;
+	Animator animator;
+
+	public float SpeedHero {
+		get {
+			return speedHero;
+		}
+		set {
+			speedHero = value;
+		}
+	}
+
+
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		animator.SetFloat("walk",Mathf.Abs(countMoves()));
+		rigidbody2D.velocity = new Vector2 (countMoves() * SpeedHero,rigidbody2D.velocity.y);
+		if (moves > 0 && !flipright)
+						Flip ();
+				else
+			if (moves < 0 && flipright)
+						Flip ();
+	}
+
+	float countMoves()
+	{
+		moves= Input.GetAxis ("Horizontal");
+		return moves;
+	}
+
+	void Flip()
+	{
+		flipright = false;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;   
+
 	}
 }
